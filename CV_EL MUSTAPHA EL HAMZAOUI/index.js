a747de63aeab2828f1f2faacb8ed2d6db0c29eca
aida2222222223 <136273204+aida2222222223@users.noreply.github.com>
2024-09-28 21:59:36 +0200

    Add files via upload

---
+++ CV_EL MUSTAPHA EL HAMZAOUI/index.js
@@ -0,0 +1,9 @@
+// Función para hacer scroll con la flecha
+function redirect(fragment) {
+    window.location.href = `/#${fragment}`;
+}
+
+// Función para abrir el pdf del CV
+function openPDF() {
+    window.open("./assets/CV Álvaro Sánchez Vélez.pdf");
+}

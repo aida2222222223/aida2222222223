8bf52812b5786e58fb56545b1e3f2eeab2ffa4da
aida2222222223 <136273204+aida2222222223@users.noreply.github.com>
2024-09-28 22:07:53 +0200

    Delete CV_EL MUSTAPHA EL HAMZAOUI directory

--- CV_EL MUSTAPHA EL HAMZAOUI/index.js
+++
@@ -1,9 +0,0 @@
-// Función para hacer scroll con la flecha
-function redirect(fragment) {
-    window.location.href = `/#${fragment}`;
-}
-
-// Función para abrir el pdf del CV
-function openPDF() {
-    window.open("./assets/CV Álvaro Sánchez Vélez.pdf");
-}
